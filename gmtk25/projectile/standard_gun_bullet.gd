4d3b640450ea6dcd5b008903aa6b9a9e18423af8
Drew M <yellow_six@icloud.com>
2025-08-02 14:44:18 -0500

    started grinding

--- gmtk25/projectile/standard_gun_bullet.gd
+++ gmtk25/projectile/standard_gun_bullet.gd
@@ -1,26 +1,38 @@
 extends Projectile
 class_name TurretBullet
 
-var dying : bool = false
+@onready var raycast: RayCast2D = $RayCast2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+var dying: bool = false
 
 func extinguish() -> void:
-	$AnimationPlayer.play("blow up")
+	animation_player.play("blow up")
 	dying = true
-	
-	
-func update_position(delta) -> void:
-	if !dying:
-		position += velocity * delta
-	else:
+
+func update_position(delta: float) -> void:
+	if dying:
 		velocity = Vector2.ZERO
-	
+		return
+
+	# Set raycast direction and length based on velocity
+	#raycast.target_position = velocity * delta
+	raycast.force_raycast_update()
+
+	if raycast.is_colliding():
+		var hit = raycast.get_collider()
+		if hit and hit != wielder:
+			effect_body(hit)
+			if extinguish_on_effect_body:
+				extinguish()
+	else:
+		position += velocity * delta
 
 func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	queue_free()
-	
-	
-func effect_body(body : Node2D) -> void:
-	var extinguish_triggered : bool = false
+
+func effect_body(body: Node2D) -> void:
+	var extinguish_triggered: bool = false
 	if body.is_in_group("damageable"):
 		body.take_damage(damage, damage_type)
 		hit_entities.append(body)
